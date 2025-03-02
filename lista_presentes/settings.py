38a28cf7e74ed37b5863cdee28d1532353e5c86c
Gabriel Barroso Faustino Gomes <b.rrosocode@gmail.com>
2025-03-01 23:00:01 -0300

    Resolvendo erro de cors

--- lista_presentes/settings.py
+++ lista_presentes/settings.py
@@ -42,17 +42,24 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'lista_presentes_api'
+    'lista_presentes_api',
+    'corsheaders' # Para resolver erro de cors
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware' # Para resolver erro de cors
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    # "https://seu-front-end.com",
+    "http://localhost:3000",  # Para ambiente de desenvolvimento
 ]
 
 ROOT_URLCONF = 'lista_presentes.urls'
