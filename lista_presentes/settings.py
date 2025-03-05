b8887dc0ca0c1a99381b614c3ec377e0aaec990f
Gabriel Barroso Faustino Gomes <b.rrosocode@gmail.com>
2025-03-05 10:09:09 -0300

    Adicionando configuração da pasta media para ver se consigo salvar as imagens adequadamente

--- lista_presentes/settings.py
+++ lista_presentes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os # Corrigindo erro nas imagens
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,3 +155,7 @@ CORS_ALLOW_HEADERS = [
     "authorization",
     "x-csrf-token",
 ]
+
+# Adicionando correções para lidar com imagens corretamente
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
